492a067fa9cd296171484ab362b24da376699386
tony muema <tonymwemar@gmail.com>
2022-05-23 12:43:35 +0300

    Added todo list

---
+++ eth-todo-list/contracts/TodoList.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.5.0;
+
+
+contract TodoList {
+    uint public taskCount = 0;
+
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+       createTask(
+           "Welcome to Solidity"
+       ); 
+    }
+    
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount,_content, false);
+    }
+
+
+}
+
+
